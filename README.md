9402c11b3233096ee5f512f42f68be3c07756f61
Kairixir <kairixir@protonmail.com>
2025-04-18 13:58:19 +0200

    feat: add web server

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Exemplar CI/CD task for Delta team interview.
   - [Getting started](https://go.dev/doc/tutorial/getting-started)
   - [Create module](https://go.dev/doc/tutorial/create-module)
   - [learnxinyminutes - Go](https://learnxinyminutes.com/go/)
+  - [fmt](https://pkg.go.dev/fmt@go1.24.2)
+  - [log](https://pkg.go.dev/log@go1.24.2)
+  - [net/http](https://pkg.go.dev/net/http@go1.24.2)
+  - [os](https://pkg.go.dev/os@go1.24.2)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+)
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+    log.Print("hello-world: received a request")
+    fmt.Fprintf(writer, "Hello World!\n")
+}
+
+func main() {
+    log.Print("hello-world: starting server...")
+    http.HandleFunc("/", handler)
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
+    log.Printf("hello-world: listening on port %s", port)
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
