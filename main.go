266642992b08f57312d4e26e0e6452672df4f2e8
Kairixir <kairixir@protonmail.com>
2025-04-18 14:29:37 +0200

    style: format main.go

--- main.go
+++ main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "os"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func handler(writer http.ResponseWriter, req *http.Request) {
-    log.Print("hello-world: received a request")
-    fmt.Fprintf(writer, "Hello World!\n")
+	log.Print("hello-world: received a request")
+	fmt.Fprintf(writer, "Hello World!\n")
 }
 
 func main() {
-    log.Print("hello-world: starting server...")
-    http.HandleFunc("/", handler)
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
-    log.Printf("hello-world: listening on port %s", port)
-    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	log.Print("hello-world: starting server...")
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Printf("hello-world: listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
-
